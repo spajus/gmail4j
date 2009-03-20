28f7d43e47da05af79bbbb67b8bce514de2f6c06
tomas.varaneckas <tomas.varaneckas@3bcb6f58-062d-11de-a42d-873b86d837ff>
2009-03-20 11:24:35 +0000

    Comments
    git-svn-id: http://gmail4j.googlecode.com/svn/trunk@9 3bcb6f58-062d-11de-a42d-873b86d837ff

--- src/test/java/com/googlecode/gmail4j/rss/test/RssGmailClientTest.java
+++ src/test/java/com/googlecode/gmail4j/rss/test/RssGmailClientTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2008-2009 Tomas Varaneckas
+ * http://www.varaneckas.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.googlecode.gmail4j.rss.test;
 
 import static org.junit.Assert.assertNotNull;
@@ -12,9 +29,20 @@ import org.junit.Test;
 import com.googlecode.gmail4j.GmailClient;
 import com.googlecode.gmail4j.GmailException;
 import com.googlecode.gmail4j.GmailMessage;
+import com.googlecode.gmail4j.auth.Credentials;
 import com.googlecode.gmail4j.rss.RssGmailClient;
+import com.googlecode.gmail4j.rss.RssGmailMessage;
 import com.googlecode.gmail4j.util.LoginDialog;
 
+/**
+ * {@link RssGmailClient} tests
+ * 
+ * @see RssGmailClient
+ * @see RssGmailMessage
+ * @see Credentials
+ * @author Tomas Varaneckas &lt;tomas.varaneckas@gmail.com&gt;
+ * @version $Id$
+ */
 public class RssGmailClientTest {
 
     private static final Log log = LogFactory.getLog(RssGmailClientTest.class);
